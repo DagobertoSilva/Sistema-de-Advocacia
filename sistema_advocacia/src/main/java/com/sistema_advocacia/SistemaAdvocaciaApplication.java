93111dffe4c29190294f7d42d7da9939f0e8e930
MatheusSalesEngComp <matsales.eng@gmail.com>
2026-06-09 15:44:17 -0300

    feat: adicionar classe principal para inicialização do Spring Boot

---
+++ sistema_advocacia/src/main/java/com/sistema_advocacia/SistemaAdvocaciaApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_advocacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAdvocaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAdvocaciaApplication.class, args);
+	}
+
+}
