b63b6ce6c2a167ef84ea2525065fb728734c65f0
MatheusSalesEngComp <matsales.eng@gmail.com>
2026-06-09 18:35:16 -0300

    feat: cria UsuarioRepository com busca por login

--- sistema_advocacia/src/main/java/com/sistema_advocacia/repository/UsuarioRepository.java
+++ sistema_advocacia/src/main/java/com/sistema_advocacia/repository/UsuarioRepository.java
@@ -1,5 +1,14 @@
 package com.sistema_advocacia.repository;
 
-public class UsuarioRepository {
+import com.sistema_advocacia.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     
+    Optional<Usuario> findByLogin(String login);
+
 }
