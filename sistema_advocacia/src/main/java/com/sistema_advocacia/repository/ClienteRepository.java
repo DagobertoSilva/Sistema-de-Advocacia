a065e6989e3cec6b11f0e83886a8dd89d5d1e638
MatheusSalesEngComp <matsales.eng@gmail.com>
2026-06-09 18:34:38 -0300

    feat: cria ClienteRepository com busca por whatsapp

--- sistema_advocacia/src/main/java/com/sistema_advocacia/repository/ClienteRepository.java
+++ sistema_advocacia/src/main/java/com/sistema_advocacia/repository/ClienteRepository.java
@@ -1,5 +1,14 @@
 package com.sistema_advocacia.repository;
 
-public class ClienteRepository {
+import com.sistema_advocacia.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     
+    Optional<Cliente> findByNumeroWhatsapp(String numeroWhatsapp);
+
 }
