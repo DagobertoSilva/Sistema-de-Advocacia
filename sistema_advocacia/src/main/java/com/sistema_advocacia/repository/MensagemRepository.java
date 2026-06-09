04a01e603a17e60226dd3bb91565f05ece5d4a07
MatheusSalesEngComp <matsales.eng@gmail.com>
2026-06-09 18:34:58 -0300

    feat: cria MensagemRepository com busca por id do cliente

--- sistema_advocacia/src/main/java/com/sistema_advocacia/repository/MensagemRepository.java
+++ sistema_advocacia/src/main/java/com/sistema_advocacia/repository/MensagemRepository.java
@@ -1,5 +1,14 @@
 package com.sistema_advocacia.repository;
 
-public class MensagemRepository {
+import com.sistema_advocacia.model.Mensagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
     
+    List<Mensagem> findByClienteId(Long clienteId);
+
 }
