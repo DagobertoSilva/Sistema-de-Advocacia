8e07d88a58fd0150f19b8cd411241b4a05034a65
MatheusSalesEngComp <matsales.eng@gmail.com>
2026-06-09 15:23:38 -0300

    feat: mapear entidade Usuario para controle de credenciais

---
+++ sistema_advocacia/src/main/java/com/sistema_advocacia/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.sistema_advocacia.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario") // Nome da tabela em minúsculo para o Postgres
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String login;
+
+    @Column(nullable = false, length = 255)
+    private String senha;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+}
