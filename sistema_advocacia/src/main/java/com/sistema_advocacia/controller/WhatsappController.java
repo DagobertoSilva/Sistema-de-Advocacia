302b265bdb640c7c2e73411705bc1eff40ea0692
MatheusSalesEngComp <matsales.eng@gmail.com>
2026-06-09 18:47:17 -0300

    feat: cria WhatsappController para gerenciar mensagens do webhook do chatbot

--- sistema_advocacia/src/main/java/com/sistema_advocacia/controller/WhatsappController.java
+++ sistema_advocacia/src/main/java/com/sistema_advocacia/controller/WhatsappController.java
@@ -1,6 +1,6 @@
 //Rota que o Node.js do seu colega vai chamar para salvar os leads da IA
 package com.sistema_advocacia.controller;
 
-public class IntegracaoController {
+public class WhatsappController {
     
 }
