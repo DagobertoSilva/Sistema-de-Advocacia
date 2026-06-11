c3321e7b68606210a432cab3d24559f9436027c9
DagobertoSilva <daagobertosilva@gmail.com>
2026-06-11 02:53:21 -0300

    Criado Tela de atendimentosMes e DesempenhoChatBot

--- Advocacia-Project/src/App.jsx
+++ Advocacia-Project/src/App.jsx
@@ -2,6 +2,9 @@ import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import TabelaCasos from "./components/TabelaCasos";
 import Login from "./components/Login";
+import AtendimentosMes from "./components/AtendimentosMes";
+import DistribuicaoCasos from "./components/DistribuicaoCasos";
+import DesempenhoChatbot from "./components/DesempenhoChatbot";
 import "./App.css";
 
 function App() {
@@ -16,7 +19,49 @@ function App() {
   );*/   // Descomentar para chamar a página cliente
 
   //Chamando a página do Login
-  return <Login />;
+  //return <Login />;
+ 
+  /*return (
+      /*<div className="container">
+      <Sidebar />
+      <main className="content">
+        <Header />
+        <div style={{ padding: "30px", background: "#f5f5f5" }}>
+      <AtendimentosMes />
+    </div>
+      </main>
+    </div>
+  );*/
+
+  //chamaando Relatorios
+  return (
+    <div className="container">
+      <Sidebar />
+      <main className="content">
+        <Header />
+        <div style={{ padding: "30px", background: "#f5f5f5" }}>
+      <AtendimentosMes />
+    </div>
+
+    <div
+      style={{
+        padding: "30px",
+        background: "#f5f5f5",
+        minHeight: "100vh",
+      }}
+    >
+      <DistribuicaoCasos />
+    </div>
+
+    <div>
+      <DesempenhoChatbot />
+    </div>
+      </main>
+    </div>
+  );
+
+
+
 }
 
 export default App;
