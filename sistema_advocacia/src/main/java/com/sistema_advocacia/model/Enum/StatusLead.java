316b9d468e07cff84aa745377dc6cbe0efa52c4d
Diogo Di Torres <diogoditorres@gmail.com>
2026-07-12 20:42:33 -0300

    Correção StatusLead
    Correção do problema número 11

--- sistema_advocacia/src/main/java/com/sistema_advocacia/model/Enum/StatusLead.java
+++ sistema_advocacia/src/main/java/com/sistema_advocacia/model/Enum/StatusLead.java
@@ -1,9 +1,28 @@
 package com.sistema_advocacia.model.Enum;
 
 public enum StatusLead {
-    Em_triagem,
-    Emergencia_max,
-    Aguardando_retorno,
-    Contrato_fechado,
-    Encerrado
+    Em_triagem(1),
+    Emergencia_max(2),
+    Aguardando_retorno(3),
+    Contrato_fechado(4),
+    Encerrado(5);
+
+    private final int idFrontend;
+
+    StatusLead(int idFrontend) {
+        this.idFrontend = idFrontend;
+    }
+
+    public static StatusLead fromId(int id) {
+        for (StatusLead status : values()) {
+            if (status.idFrontend == id) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("ID de status inválido: " + id);
+    }
+
+    public int getIdFrontend() {
+        return idFrontend;
+    }
 }
